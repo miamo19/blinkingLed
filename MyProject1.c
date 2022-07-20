a34e522bfa04079084f8596be9efb38f277b52ed
Hyacinthe <80036233+miamo19@users.noreply.github.com>
2022-07-20 19:12:13 +0100

    blinker

---
+++ MyProject1.c
@@ -0,0 +1,32 @@
+void main()
+{
+ /////// Initializations ///////
+TRISB = 0x00; // setting PORTB as output
+TRISA = 0x1F; // all PORTA is set as input
+
+/////// Definition of the endless loop //// ///////
+do
+ {
+ //Program to carry out
+ PORTB = 0b00000001; // PORTB high
+ Delay_ms(500); // 500 milliseconds delay
+ PORTB = 0b00000000; // all PORTB off
+ Delay_ms(500);
+ PORTB = 0b00000011;
+ Delay_ms(500);
+ PORTB = 0b00000111;
+ Delay_ms(500);
+ PORTB = 0b00001111;
+ Delay_ms(500);
+ PORTB = 0b00011111;
+ Delay_ms(500);
+ PORTB = 0b00111111;
+ Delay_ms(500);
+ PORTB = 0b01111111;
+ Delay_ms(500);
+ PORTB = 0b11111111;
+ Delay_ms(500);
+ PORTB = 0b00000000;
+ Delay_ms(500); // 500 milliseconds delay
+ } while(1); // endless loop
+} // en
---
+++ MyProject1.cp
@@ -0,0 +1,33 @@
+#line 1 "C:/projectMicrocontroler/mikroCpro1/MyProject1.c"
+void main()
+{
+
+TRISB = 0x00;
+TRISA = 0x1F;
+
+
+do
+ {
+
+ PORTB = 0b00000001;
+ Delay_ms(500);
+ PORTB = 0b00000000;
+ Delay_ms(500);
+ PORTB = 0b00000011;
+ Delay_ms(500);
+ PORTB = 0b00000111;
+ Delay_ms(500);
+ PORTB = 0b00001111;
+ Delay_ms(500);
+ PORTB = 0b00011111;
+ Delay_ms(500);
+ PORTB = 0b00111111;
+ Delay_ms(500);
+ PORTB = 0b01111111;
+ Delay_ms(500);
+ PORTB = 0b11111111;
+ Delay_ms(500);
+ PORTB = 0b00000000;
+ Delay_ms(500);
+ } while(1);
+}
