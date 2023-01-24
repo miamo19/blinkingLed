02eee44f326acc18550ab60ffbcfb060aba5fc7e
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-01-24 05:35:39 +0100

    Update MyProject1.c

--- MyProject1.c
+++ MyProject1.c
@@ -32,5 +32,15 @@ do
  Delay_ms(500);
  PORTB = 0b00000011;
  Delay_ms(500); // 500 milliseconds delay
+ PORTB = 0b00000111;
+ Delay_ms(500);
+ PORTB = 0b00001111;
+ Delay_ms(500);
+ PORTB = 0b00011111;
+ Delay_ms(500);
+ PORTB = 0b00111111;
+ Delay_ms(500);
+ PORTB = 0b01111111;
+ Delay_ms(500);
  } while(1); // endless loop
 } // end
